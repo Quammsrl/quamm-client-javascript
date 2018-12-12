4d0f6a5cdb08114ec0ff8d282193cbfb2c8aac92
Marco 'Gatto' Boffo <marcoboffo.waves@gmail.com>
2018-12-12 14:33:47 +0100

    postman lib

---
+++ src/postman.js
@@ -0,0 +1,31 @@
+const { MachineAuth } = require('./auth')
+const axios = require('axios')
+
+const sendDirectusMail = async (caller, auth, postmanService = 'https://postman.quamm.it') => {
+  const login = await MachineAuth.getAuth(Object.assign({}, auth, { audience: postmanService }))
+  if (
+    typeof login.accessToken === 'undefined' ||
+    typeof login.tokenType === 'undefined' ||
+    !login.scope.includes('create:mail')) throw new Error('Bad credetials.')
+
+  try {
+    const { data } = await axios({
+      url: `${postmanService}/webhooks/directus/${caller}`,
+      method: 'post',
+      headers: {
+        Authorization: `${login.tokenType} ${login.accessToken}`,
+        'Content-Type': 'application/json'
+      }
+    })
+
+    return data.status === 'pass'
+  } catch (e) {}
+  return false
+}
+
+module.exports = {
+  install,
+  getToken,
+  saveToken,
+  verifyToken
+}
