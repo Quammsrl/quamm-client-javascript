f6920b02713c3bbe41ac6667b99a0bb968667158
Davide Marchet <io@davidemarchet.it>
2019-02-07 17:19:03 +0100

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,16 @@ import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+const dependencies = Object.keys(require('./package.json').dependencies)
 
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/quamm-client-javascript.js',
-    format: 'cjs'
+    format: 'cjs',
+    export: 'named'
   },
+  external: [ 'http', 'https', 'url', 'assert', 'stream', 'tty', 'util', 'os', 'zlib' ].concat(dependencies),
   plugins: [
     resolve(),
     commonjs(),
